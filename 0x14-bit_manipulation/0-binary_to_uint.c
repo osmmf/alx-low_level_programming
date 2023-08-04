eef4c047d18493b1c2f6164a4cee0591d64c91ed
”osmmf” <elmeftahioussama@gmail.com>
2023-08-04 04:22:28 -0700

    m

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	unsigned int num = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	while (*b)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (*b != '0' && *b != '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		num = 2 * num + (*b++ - '0');
 	}
-
-	return (dec_val);
+	return (num);
 }
